2607022767e07aa88e3f43a8e474bbb592f12327
hamaup <32893785+hamaup@users.noreply.github.com>
2023-02-15 01:02:09 +0900

    Lesson6 complated

--- src/container/vwbl-container.js
+++ src/container/vwbl-container.js
@@ -6,74 +6,77 @@ import { ManageKeyType, UploadContentType, UploadMetadataType, VWBL } from 'vwbl
 const useVWBL = () => {
   const [userAddress, setUserAddress] = useState('');
   const [web3, setWeb3] = useState();
-  /* useVWBLの持つ変数にvwblインスタンスを追加 */
+  const [vwbl, setVwbl] = useState();
 
   const connectWallet = async () => {
     try {
-      // メタマスクがインストールされているか確認
+      // windowオブジェクトからethereumオブジェクトを抽出
       const { ethereum } = window;
+
+      // ethereumオブジェクトの有無を確認
       if (!ethereum) {
         throw new Error('Please install MetaMask!');
       } else {
         console.log('MetaMask is installed!', ethereum);
       }
 
-      // ウォレット接続して、userAddressとweb3インスタンスのstateを保存
-      await ethereum.request({ method: 'eth_requestAccounts' });
+      // ウォレットに接続
+      await ethereum.send('eth_requestAccounts');
+
+      // web3インスタンスの生成
       const web3 = new Web3(ethereum);
+
+      // ユーザーアドレスを取得
       const accounts = await web3.eth.getAccounts();
       const currentAccount = accounts[0];
+
+      // 各変数のstateを保存
       setWeb3(web3);
       setUserAddress(currentAccount);
 
-      // 接続しているチェーンがMumbaiでない場合は、チェーンの変更をrequest
-      const connectedChainId = await web3.eth.getChainId();
-      const properChainId = parseInt(process.env.REACT_APP_CHAIN_ID);
-      if (connectedChainId !== properChainId) {
-        await ethereum.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: web3.utils.toHex(properChainId) }],
-        });
-      }
+      // initVwblを実行してvwblインスタンスを作成する
+      initVwbl(web3);
 
-      /* vwblインスタンス作成してstateを保存する処理 */
-      // initVwbl(web3);
     } catch (error) {
+
       if (error.code === 4001) {
+        // ユーザーが接続を拒否したとき
         alert('Please connect Your Wallet.');
       } else {
+        // ethreumオブジェクトが確認できなかった時など
         alert(error.message);
       }
+
+      // エラー内容を開発者コンソールに表示
       console.error(error);
     }
   };
-
   const disconnectWallet = () => {
     setUserAddress('');
     setWeb3(undefined);
-    // setVwbl(undefined);
+    setVwbl(undefined);
   };
 
   // Lesson-3
   const initVwbl = (web3) => {
-    // // vwblインスタンスの作成
-    // const vwblInstance = new VWBL({
-    //   web3,
-    //   contractAddress: process.env.REACT_APP_NFT_CONTRACT_ADDRESS,
-    //   vwblNetworkUrl: process.env.REACT_APP_VWBL_NETWORK_URL,
-    //   manageKeyType: ManageKeyType.VWBL_NETWORK_SERVER,
-    //   uploadContentType: UploadContentType.IPFS,
-    //   uploadMetadataType: UploadMetadataType.IPFS,
-    //   ipfsNftStorageKey: process.env.REACT_APP_NFT_STORAGE_KEY,
-    // });
-    // // vwblインスタンスをstateを保存
-    // setVwbl(vwblInstance);
+    // vwblインスタンスの作成
+    const vwblInstance = new VWBL({
+      web3,
+      contractAddress: process.env.REACT_APP_NFT_CONTRACT_ADDRESS,
+      vwblNetworkUrl: process.env.REACT_APP_VWBL_NETWORK_URL,
+      manageKeyType: ManageKeyType.VWBL_NETWORK_SERVER,
+      uploadContentType: UploadContentType.IPFS,
+      uploadMetadataType: UploadMetadataType.IPFS,
+      ipfsNftStorageKey: process.env.REACT_APP_NFT_STORAGE_KEY,
+    });
+    // vwblインスタンスをstateを保存
+    setVwbl(vwblInstance);
   };
 
   return {
     userAddress,
     web3,
-    // vwbl,
+    vwbl,
     connectWallet,
     disconnectWallet,
     initVwbl,
